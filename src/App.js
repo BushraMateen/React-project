fa931f729f4d8765fc54cd99a3e537652ea8c108
mastanpatel <Bushra.mateen95@outlook.com>
2021-11-19 22:40:18 +0530

    added greet component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
+import Greet from './Components/Greet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Greet name="Bushra"/>
+        <Greet name = "Mateen "/>
+      </div>
+    );
+  }
 }
-
 export default App;

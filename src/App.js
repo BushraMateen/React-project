b4812fa985d2e7ea2ff130aeffa230dfe88d44e2
mastanpatel <Bushra.mateen95@outlook.com>
2021-11-20 18:49:57 +0530

    Added message component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import Greet from './Components/Greet'
-import Welcome from './Components/Greet'
+// import Greet from './Components/Greet'
+// import Welcome from './Components/Welcome'
+import Message from './Components/Message'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <Greet name="Bruce" heroname="batman"/>
+        <Message />
+        {/* <Greet name="Bruce" heroname="batman"/>
         <Greet name = "Bruce"  heroname="superman" />
         <Welcome name="Bruce" heroname="batman"/>
-        <Welcome name = "Bruce"  heroname="superman" />
+        <Welcome name = "Bruce"  heroname="superman" /> */}
+     
       </div>
     );
   }

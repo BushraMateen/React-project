59f3eb0206a831b6802e0f5b47d60fd6bbf6d6ea
mastanpatel <Bushra.mateen95@outlook.com>
2021-11-22 12:39:57 +0530

    added EventBind component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import Message from './Components/Message'
 import Counter from './Components/Counter'
 import FunctionClick from './Components/FunctionClick'
 import ClassClick from './Components/ClassClick'
+import EventBind from './Components/EventBind'
+
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <FunctionClick />
-        <ClassClick />
+        {/* <FunctionClick />
+        <ClassClick /> */}
+        <EventBind />
         {/* <Message /> */}
         {/* <Counter /> */}
         {/* <Greet name="Bruce" heroname="batman"/>

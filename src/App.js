55119cf1a1736b6140f1f4e2d508aa656707251e
mastanpatel <Bushra.mateen95@outlook.com>
2021-11-25 11:30:51 +0530

    Fragments

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import './appStyles.css'
 import styles from './appStyles.module.css'
 import Form from './Components/Form'
 import LifecycleA from './Components/LifecycleA';
+import FragmentDemo from './Components/FragmentDemo';
+import Table from './Components/Table';
 
 
 
@@ -24,7 +26,9 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <LifecycleA />
+        <Table />
+        {/* <FragmentDemo /> */}
+        {/* <LifecycleA /> */}
         {/* <Form /> */}
         {/* <h1 className='error'>Error</h1>
         <h1 className={styles.success}>Success</h1> */}
